e0a89f5d32877aa8b7380b55de76be63a66b5a2e
weslleyalessi <105368330+weslleyalessi@users.noreply.github.com>
2023-10-03 11:23:15 -0300

    ok

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 function tocaSomPom(){
     /*document.querySelector('#som_tecla_pom').play();*/
-    document.querySelector(".tecla_pom").onclick = som_tecla_pom;
+    document.querySelector(".tecla_pom").onclick = tocaSomPom;
 }
