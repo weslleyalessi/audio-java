9e61583fb23b7b67a3a192f6558948f94ebcd70e
weslleyalessi <105368330+weslleyalessi@users.noreply.github.com>
2023-10-10 11:03:11 -0300

    v1

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
+document.querySelectorAll(.tecla);
+
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
-    document.querySelector(".tecla_pom").onclick = tocaSomPom;
+    document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 function tocaSomClap(){
     document.querySelector('#som_tecla_clap').play();
     }
     document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+function tocaSomTim(){
+    document.querySelector('#som_tecla_tim').play();
+    }
+    document.querySelector('.tecla_tim').onclick = tocaSomTim;
