2a2dcf305cdf50eda87f3f8b3ed0431f6726b05a
weslleyalessi <105368330+weslleyalessi@users.noreply.github.com>
2023-10-24 11:07:11 -0300

    aef

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 //criando/declarando a função tocaSomPom
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(IDaudio){
+    document.querySelector(IDaudio).play();
     }
     //chamando/invocando a função TocaSomPom pelo clique do botão pom
-    document.querySelector('.tecla_pom').onclick = tocaSomPom;  
+    /*document.querySelector('.tecla_pom').onclick = IDaudio;*/
 
 
 //criando referência constante ListaDeTeclas buscando todos
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //invocando a função tocaSomPom a partir do item 0, 1º botão
-listaDeTeclas [0].onclick = tocaSomPom;
+listaDeTeclas [0].onclick = IDaudio;
 
 //criando referência variável "contador" iniciando = 0
 let contador = 0; 
 
 //estrutura de repetição while = enquanto
 while(contador <9){
-    listaDeTeclas[contador].onclick = tocaSomPom;
+    listaDeTeclas[contador].onclick = IDaudio;
     contador = contador + 1;
 
     console.log(contador);
 }
+
